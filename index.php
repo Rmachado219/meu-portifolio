aa2e402af35b35c93778127e93cfb83164f48f49
Rodrigo Morais Machado <rodrigmachado11@gmail.com>
2020-08-11 17:38:58 -0300

    estruturando pastas

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- HEAD -->
+<?php
+$tituloPagina = "Português | Web Full Stack jr";
+require_once("./inc/head.php");
+?>
+<!-- HEADER -->
+<?php require_once("./inc/header.php"); ?>
+<!-- FOOTER -->
+<?php require_once("./inc/footer.php"); ?>
+
